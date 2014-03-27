1b8bf2a885499b231f553d4134420844e3240405
mikekauffman <mike.a.kauffman@gmail.com>
2014-03-26 19:36:31 -0600

    tasks can be created via a form input and added to the homepage

--- app.rb
+++ app.rb
@@ -2,12 +2,19 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  TASKS = []
+
   get '/' do
-    erb :homepage
+    erb :homepage, locals: {tasks: TASKS}
   end
 
   get '/task/new' do
     erb :new_task
+  end
+
+  post '/' do
+    TASKS << params[:new_task]
+    redirect '/'
   end
 
 end
--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -5,11 +5,13 @@ require 'capybara/rspec'
 Capybara.app = App
 
 feature 'Manage Homepage' do
-  scenario 'Welcome displays on the homepage' do
+  scenario 'Homepage displays welcome as well as user-created tasks via a form input' do
     visit '/'
     expect(page).to have_content 'Welcome'
     click_link 'Add a Task'
     fill_in 'new_task', :with => 'Cook Dinner'
     click_on 'Create Task'
+    expect(page).to have_content 'Welcome'
+    expect(page).to have_content 'Cook Dinner'
   end
 end
